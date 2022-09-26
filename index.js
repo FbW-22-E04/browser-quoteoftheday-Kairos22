2dd4bb2c132db51d14d76e409871b9ba29defa84
Kairos22 <alandiko84@gmail.com>
2022-09-26 18:50:18 +0200

    Ex3

--- index.js
+++ index.js
@@ -1,4 +1,34 @@
-const quotes = [
+const div = document.querySelector('div');
+const h1 = document.querySelector('h1');
+const input = document.querySelector('input');
+const button = document.querySelector('button');
+
+h1.classList.add('my-4')
+div.classList.add('container');
+button.classList.add(
+  'd-block',
+ 'text-white',
+  'bg-secondary',
+   'rounded-2',
+    'py-2',
+    'px-3');
+    
+input.classList.add('mb-4', 'w-100', 'p-3', 'rounded-2');
+
+// input.style.width = '100%';
+
+  input.value = randomQuoteFunc();
+
+
+button.addEventListener('click', () => {
+
+  input.value = randomQuoteFunc();
+});
+
+
+function randomQuoteFunc(){
+
+  const quotes = [
   'Our greatest weakness lies in giving up. The most certain way to succeed is always to try just one more time.',
   "Don't watch the clock; do what it does. Keep going.",
   'The secret of getting ahead is getting started.',
@@ -12,3 +42,12 @@ const quotes = [
   'There is little success where there is little laughter.',
   'We cannot solve our problems with the same thinking we used when we created them.'
 ];
+
+const randomNumber = Math.floor(Math.random()* quotes.length)
+
+return quotes[randomNumber]
+
+}
+
+
+
